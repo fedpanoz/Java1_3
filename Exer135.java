0220ccfacc805b39098475c6f4a33029a56c7739
zizza <federicopanozzo@gmail.com>
2021-08-30 06:19:38 +0200

    30/08

--- Exer135.java
+++ Exer135.java
@@ -1,3 +1,22 @@
+/* RollLoadedDie prints the result of rolling a loaded die suche the probability  are changed */
 public class Exer135 {
+    public static void main(String[] args){
+        double a = Math.random();
+        int roll;
+        if(a < 1.0 / 8.0)
+            roll = 1;
+        else if(a < 2.0 / 8.0)
+            roll = 2;
+        else if(a < 3.0 / 8.0)
+            roll = 3;
+        else if (a < 4.0 / 8.0)
+            roll = 4;
+        else if (a < 5.0 / 8.0)
+            roll = 5;
+        else roll = 6;
+        System.out.println("Rolling die " + roll);
+
+
+    }
 
 }
---
+++ daButtare.java
@@ -0,0 +1,7 @@
+public class daButtare {
+    public static void main(String[] args){
+        int i, j;
+        for (i = 0, j = 0; i < 10; i++ )
+            System.out.println(j);
+    }
+}
