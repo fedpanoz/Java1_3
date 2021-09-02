dccc74af257c09491c17213fa227da91c09ab47a
federico <federicopanozzo@gmail.com>
2021-09-02 06:09:58 +0200

    02.09

---
+++ Exer1_3_16.java
@@ -0,0 +1,15 @@
+public class Exer1_3_16 {
+    public static void main(String[] args){
+        int n = Integer.parseInt(args[0]);
+        for(int i = 1, j = 1; j <= n; i++){
+            if(j <= n){
+                System.out.println(j);
+                j *= 2;
+
+
+            }
+
+
+        }
+    }
+}
